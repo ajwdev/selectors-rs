a4d1c3a1cd52992bee10c7e7348874a729c47b8a
Andrew Williams <williams.andrew@gmail.com>
2018-06-17 15:22:19 -0500

    First pass macro-izing test cases

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,90 +5,116 @@ pub use self::selector::SelectorParser;
 mod tests {
     use super::selector::*;
 
+    macro_rules! parse_ok {
+        ($p:ident, $e:expr) => {
+            match $p.parse($e) {
+                // This arm won't be needed once we're done testing parser "by hand"
+                Ok(ast) => { println!("== ast for \"{}\" -> {:?}", $e, ast); }
+                Err(why) => { panic!("error with \"{}\", line {}: {:?}", $e, line!(), why); }
+            }
+        }
+    }
+
+    macro_rules! parse_err {
+        ($p:ident, $e:expr) => {
+            if let Ok(ast) = $p.parse($e) {
+                panic!("expected error with \"{}\", line {}; ast: {:?}", $e, line!(), ast);
+            }
+        }
+    }
+
     #[test]
     fn selector_label_keys() {
         let p = LabelKeyParser::new();
 
-        assert!(p.parse("f").is_ok());
-        assert!(p.parse("fo").is_ok());
-        assert!(p.parse("foo").is_ok());
-        assert!(p.parse("example.com/foo").is_ok());
-        assert!(p.parse("foo_bar").is_ok());
-        assert!(p.parse("foo.bar").is_ok());
+        parse_ok!(p, "f");
+        parse_ok!(p, "fo");
+        parse_ok!(p, "foo");
+        parse_ok!(p, "example.com/foo");
+        parse_ok!(p, "foo_bar");
+        parse_ok!(p, "foo.bar");
 
-        assert!(p.parse("foo bar").is_err());
-        assert!(p.parse("(foo)").is_err());
-        assert!(p.parse("-foo").is_err());
-        assert!(p.parse(".foo").is_err());
-        assert!(p.parse("_foo").is_err());
-        assert!(p.parse("bar/example.com/foo").is_err());
+        parse_err!(p, "foo bar");
+        parse_err!(p, "(foo)");
+        parse_err!(p, "-foo");
+        parse_err!(p, ".foo");
+        parse_err!(p, "_foo");
+        parse_err!(p, "baz/baz/foo");
+        parse_err!(p, "bar/example.com/foo");
     }
 
     #[test]
     fn selector_label_values() {
         let p = LabelValueParser::new();
 
-        assert!(p.parse("f").is_ok());
-        assert!(p.parse("fo").is_ok());
-        assert!(p.parse("foo").is_ok());
-        assert!(p.parse("foo_bar").is_ok());
-        assert!(p.parse("foo.bar").is_ok());
+        parse_ok!(p, "f");
+        parse_ok!(p, "fo");
+        parse_ok!(p, "foo");
+        parse_ok!(p, "foo_bar");
+        parse_ok!(p, "foo.bar");
 
-        assert!(p.parse("example.com/foo").is_err());
-        assert!(p.parse("foo bar").is_err());
-        assert!(p.parse("(foo)").is_err());
-        assert!(p.parse("-foo").is_err());
-        assert!(p.parse(".foo").is_err());
-        assert!(p.parse("_foo").is_err());
-        assert!(p.parse("bar/example.com/foo").is_err());
+        parse_err!(p, "example.com/foo");
+        parse_err!(p, "foo bar");
+        parse_err!(p, "(foo)");
+        parse_err!(p, "-foo");
+        parse_err!(p, ".foo");
+        parse_err!(p, "_foo");
+        parse_err!(p, "baz/baz/foo");
     }
 
     #[test]
     fn selector_label_values_group() {
         let p = LabelValueGroupParser::new();
 
-        assert!(p.parse("(foo)").is_ok());
-        assert!(p.parse("(foo,)").is_ok());
-        assert!(p.parse("(foo,bar,baz)").is_ok());
-        assert!(p.parse("(foo,bar,baz,)").is_ok());
-        assert!(p.parse("foo,bar,baz,").is_err());
+        parse_ok!(p, "(foo)");
+        parse_ok!(p, "(foo,)");
+        parse_ok!(p, "(foo,bar,baz)");
+        parse_ok!(p, "(foo,bar,baz,)");
+        parse_ok!(p, "()");
+
+        parse_err!(p, "foo,bar,baz,");
     }
 
     #[test]
     fn selector_expressions() {
         let p = ExprParser::new();
 
-        assert!(p.parse("env in (qa)").is_ok());
-        assert!(p.parse("env notin (qa)").is_ok());
+        parse_ok!(p, "env in (qa)");
+        parse_ok!(p, "env notin (qa)");
     }
 
     #[test]
     fn selector_lexing() {
         let p = SelectorParser::new();
 
-        assert!(p.parse("foo").is_ok());
-        assert!(p.parse("foo_bar").is_ok());
-        assert!(p.parse("foo.bar").is_ok());
+        parse_ok!(p, "foo");
+        parse_ok!(p, "foo_bar");
+        parse_ok!(p, "foo.bar");
 
-        assert!(p.parse("foo = bar").is_ok());
-        assert!(p.parse("foo == bar").is_ok());
-        assert!(p.parse("foo != bar").is_ok());
+        parse_ok!(p, "foo = bar");
+        parse_ok!(p, "foo == bar");
+        parse_ok!(p, "foo != bar");
 
-        assert!(p.parse("foo=bar").is_ok());
-        assert!(p.parse("foo==bar").is_ok());
-        assert!(p.parse("foo!=bar").is_ok());
+        parse_ok!(p, "foo=bar");
+        parse_ok!(p, "foo==bar");
+        parse_ok!(p, "foo!=bar");
 
-        assert!(p.parse("foo= bar").is_ok());
-        assert!(p.parse("foo== bar").is_ok());
-        assert!(p.parse("foo!= bar").is_ok());
+        parse_ok!(p, "foo= bar");
+        parse_ok!(p, "foo== bar");
+        parse_ok!(p, "foo!= bar");
 
-        //assert!(SelectorParser::new().parse("").is_ok());
+        parse_err!(p, "foo baz = bar");
 
-        assert!(p.parse("foo baz = bar").is_err());
-        assert!(p.parse("foo = ").is_err());
+        parse_ok!(p, "env in (qa),app!=foo,!bar");
 
-        println!("{:?}", p.parse("env in (qa),app!=foo,!bar").unwrap());
-        assert!(p.parse("env in (qa),app!=foo,!bar").is_ok());
+    }
+
+    #[test]
+    #[ignore]
+    fn empty_labels() {
+        let p = SelectorParser::new();
+
+        parse_ok!(p, "foo=");
     }
 
     #[test]
@@ -96,9 +122,9 @@ mod tests {
     fn set_operators_as_keys_and_labels() {
         let p = SelectorParser::new();
 
-        assert!(p.parse("foo = notin").is_ok());
-        assert!(p.parse("notin = foo").is_ok());
-        assert!(p.parse("foo = in").is_ok());
-        assert!(p.parse("in = foo").is_ok());
+        parse_ok!(p, "foo = notin");
+        parse_ok!(p, "notin = foo");
+        parse_ok!(p, "foo = in");
+        parse_ok!(p, "in = foo");
     }
 }
